3730e7630f8352f3dd1da01166ff547e81e57091
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-10-13 15:22:17 -0400

    Création de category-cours.php
    Création de category-cours.php et ajout du contenu de base

---
+++ category-cours.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package igc31w
+ */
+
+get_header();
+?>
+	<h1>category-cours.php</h1>
+	<main class="site__main">
+
+		<?php
+		if ( have_posts() ) :
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				$titreOg = get_the_title();
+				$codeCours = substr($titreOg,0,7);
+				$heuresCours = substr($titreOg, strrpos($titreOg, "("));
+				$titre = substr($titreOg, strpos($titreOg," "), strrpos($titreOg,"(") - strlen($titreOg));
+			?>
+				<h2><?= $titre ?></h2>
+				<blockquote><?= wp_trim_words(get_the_excerpt(),50,"<a href='".get_the_permalink()."'>Suite</a>"); ?></blockquote>
+				<pre><?= the_category(); ?></pre>
+				<pre><?= the_date(); ?></pre>
+				<pre><?= get_the_author(); ?></pre>
+			<?php
+			endwhile;
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
