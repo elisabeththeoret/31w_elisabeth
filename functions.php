195bbc07f6b40a7391f33064fc8181d1fd14cda6
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-11-03 16:35:01 -0400

    c11- Création de la base d'une sidebar
    Ajout d'une fonction pour la sidebar dans function.php.
    Ajout du fichier sidebar-primary.php avec le commentaire de la documentation
    
    c11

--- functions.php
+++ functions.php
@@ -121,22 +121,21 @@ add_action( 'after_setup_theme', 'igc31w_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-/*
-function igc31w_widgets_init() {
+function igc31w_register_sidebars() {
+	/* Register the 'primary' sidebar. */
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'igc31w' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'igc31w' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'id'            => 'primary',
+			'name'          => __( 'Primary Sidebar' ),
+			'description'   => __( 'A short description of the sidebar.' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 }
-*/
-// add_action( 'widgets_init', 'igc31w_widgets_init' );
+add_action( 'widgets_init', 'igc31w_register_sidebars' );
 
 /**
  * Enqueue scripts and styles.
