994d9c39eac1af5a820ee0e9cf90598cf9697f55
elisabeththeoret <101665871+elisabeththeoret@users.noreply.github.com>
2022-12-06 16:09:49 -0500

    c16 - Ajouter du contenu dans la grille du main de la front-page (galerie)
    Ajouter l'<article> de galerie.
    
    c16

--- front-page.php
+++ front-page.php
@@ -36,15 +36,38 @@ get_header();
 				/* Start the Loop */
 				while ( have_posts() ) :
 					the_post();
-				?>
 
-					<article class="grille__article">
-						<h2><?= get_the_title(); ?></h2>
-						<p><?= wp_trim_words( get_the_excerpt(), 20, " ..." ); ?></p>
-						<a href="<?= get_the_permalink(); ?>">En savoir plus</a>
-					</article>
+					if ( in_category( 'galerie' ) ) :
+					?>
 
-				<?php
+						<article class="grille__galerie">
+
+							<?= the_title(); ?>
+							<?= the_content(); ?>
+
+						</article>
+
+					<?php
+					endif;
+
+					if ( in_category( 'cours' ) ) :
+					?>
+
+						<article class="grille__article">
+
+							<h2><?= the_field("nom_du_cours"); ?></h2>
+							<div class="grille__infos">
+								<small><?= the_field("sigle_du_cours"); ?></small>
+								<small><?= the_field("duree_du_cours"); ?> h</small>
+							</div>
+							<p><?= wp_trim_words( get_the_excerpt(), 20, " ..." ); ?></p>
+							<a href="<?= get_the_permalink(); ?>">En savoir plus</a>
+
+						</article>
+
+						<?php
+					endif;
+
 				endwhile;
 
 			endif;
